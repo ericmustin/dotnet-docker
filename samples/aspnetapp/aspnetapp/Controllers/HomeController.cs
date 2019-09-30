cc016efea02bb9b0a0a4688964e99660c26cd6bb
ericmustin <mustin.eric@gmail.com>
2019-09-30 18:04:42 +0200

    add serilog config setup

--- samples/aspnetapp/aspnetapp/Controllers/HomeController.cs
+++ samples/aspnetapp/aspnetapp/Controllers/HomeController.cs
@@ -4,19 +4,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using aspnetapp.Models;
+using System.Configuration;
+using Serilog;
 
 namespace aspnetapp.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
+
+
+            Log.Information("This is my second debug inside of index route"); 
             return View();
         }
 
         public IActionResult About()
         {
+
+            Log.Information("This is my second debug inside of about route");
             ViewData["Message"] = "Your application description page.";
 
             return View();
@@ -24,6 +33,8 @@ namespace aspnetapp.Controllers
 
         public IActionResult Contact()
         {
+
+            Log.Information("This is my second debug inside of contact route");
             ViewData["Message"] = "Your contact page.";
 
             return View();
@@ -31,12 +42,16 @@ namespace aspnetapp.Controllers
 
         public IActionResult Privacy()
         {
+
+            Log.Information("This is my second debug inside of privacy route");
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+
+            Log.Information("This is my second debug inside of Error route");
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
--- samples/aspnetapp/aspnetapp/Program.cs
+++ samples/aspnetapp/aspnetapp/Program.cs
@@ -7,18 +7,35 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Configuration;
+using Serilog;
+using Serilog.Formatting.Compact;
+using Serilog.Settings.Configuration;
+
 
 namespace aspnetapp
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+    
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger()
+
+            Log.Information("Starting web host");
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+                .UseStartup<Startup>()
+                .UseSerilog(); // <-- Add this line;
+    }           
 }
